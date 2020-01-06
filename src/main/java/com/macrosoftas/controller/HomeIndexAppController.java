730deb5f0155eb9523cf5ea9cf37014b6a6cf616
formationjav <sanogotech@gmail.com>
2020-01-06 10:56:18 +0000

    Add @ModelAttribute

--- src/main/java/com/macrosoftas/controller/HomeIndexAppController.java
+++ src/main/java/com/macrosoftas/controller/HomeIndexAppController.java
@@ -2,6 +2,7 @@ package com.macrosoftas.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,6 +14,15 @@ public class HomeIndexAppController {
 	public String viewHomePage(Model model) {
 
 		return "index";
+	}
+	
+	/**
+	 * addAttributes() method will be the very first to run, prior to the rest of the @RequestMapping methods.
+	 * @param model
+	 */
+	@ModelAttribute
+	public void addAttributes(Model model) {
+	    model.addAttribute("msg", "Welcome to the DashBoard Spring Boot !");
 	}
 	
 	
